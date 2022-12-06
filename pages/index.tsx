e1ed869202ebc20644e7c1aa14ee1c1cda4577da
kalanjiyaVishnu <kalanjiya.vishnu01@gmail.com>
2022-12-06 11:17:25 +0530

    tailwind init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import Head from "next/head"
-import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>vishnuj</title>
         <meta name="description" content="a website made by nexjs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>hi bob</div>
+      <h1 className="text-xl font-bold underline">Hello bob!</h1>
     </div>
   )
 }
