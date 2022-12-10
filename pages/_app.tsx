4cfa94b3b2ee44174b89eed6eb1b60e97e3dd0ac
kalanjiyaVishnu <kalanjiya.vishnu01@gmail.com>
2022-12-10 20:12:21 +0530

    add utility styles depedent on comps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import "../styles/globals.css"
+import "../styles/utilities.scss"
+
 import type { AppProps } from "next/app"
 import { Header } from "../components/Header/Header"
+import Head from "next/head"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>vishnuj</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
