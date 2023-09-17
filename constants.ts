940ea2b002c7c81b2b9b68e4a32ed7a9d32b70c4
kalanjiyaVishnu <kalanjiya.vishnu01@gmail.com>
2023-09-17 20:57:34 +0530

    Content Context as Constant!

---
+++ constants.ts
@@ -0,0 +1,10 @@
+export const content = {
+  SectionWhatDoIDO: {
+    head: 'Well, What do I do?',
+    body: `I'm a software dev, specifically a full stack engineer (have a degree
+        too, in a corner around my house). Love trying out new
+        technologies/languages!. Started a few ton of side projects that i'am
+        sure i will finish all of em. In the learning period, hoping i would
+        become an expert, near future. And i'm the batman in nights.`,
+  },
+}
